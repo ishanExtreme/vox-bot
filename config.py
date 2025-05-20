aa11d3b4ad0591df5d53080e65ac705af7ac0b3a
ishanExtreme <ishanmishra.work@outlook.com>
2025-05-20 13:53:25 +0530

    env support added

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+import os
 from pathlib import Path
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent
 
@@ -7,10 +11,12 @@ INITIATING_VOICE_PATH = "static/voices/initiating.mp3"
 NOTIFICATION_SOUND_PATH = "static/voices/notification.mp3"
 NOTIFICATION_END_SOUND_PATH = "static/voices/notification_end.mp3"
 
-CUSTOM_MODEL_PATH = "Hey-vox/Hey-vox.ppn"
-ACCESS_KEY = "IDNKisYX0LHFNlmcYsLMAq/CIWpvtwB2pej066atlK9RLCDCiOz4QQ=="
+__get_env = lambda key: config.get(key, os.environ.get(key))
 
-OPENAI_KEY = "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"
+CUSTOM_MODEL_PATH = "Hey-vox/Hey-vox.ppn"
+ACCESS_KEY = __get_env("PICOVOICE_ACCESS_KEY")
+
+OPENAI_KEY = __get_env("OPENAI_API_KEY")
 
 import platform
 
@@ -25,7 +31,7 @@ import os
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "vox"
-os.environ["LANGCHAIN_API_KEY"] = "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"
+os.environ["LANGCHAIN_API_KEY"] = __get_env("LANGCHAIN_API_KEY")
 
 ## Omniparser
 ICONDETECT_WEIGHTS_PATH = str(ROOT_DIR/"static/weights/icon_detect/best.pt")
